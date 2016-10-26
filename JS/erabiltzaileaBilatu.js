88b93b78e1bb21df6b191ae3ddf08ca6b95b3798
Oier Beaskoetxea <obeaskoetxea@gmail.com>
2016-10-26 21:34:26 +0200

    lab5 bukatuta

--- JS/erabiltzaileaBilatu.js
+++ JS/erabiltzaileaBilatu.js
@@ -1,27 +1,13 @@
 function bilatu() {
-	if (window.XMLHttpRequest){ // code for IE7+, Firefox, Chrome, Opera, Safari
-	  var xml = new XMLHttpRequest();
-	  alert('0,1');
-	}else{// code for IE6, IE5
-	  var xml =  ActiveXObject("Microsoft.XMLHTTP");
-	  alert('0,2');
-	}
-	xml.open("GET",'../XML/erabiltzaileak.xml',false);
-	alert('0,4');
-	xml.send(null);
-	alert('0,5');
- 
-	var xmlText = xml.responseText;
-	var xmlResult = xml.responseXML;
-	//var xml = document.getElementById("xml").contentDocument;
-	var emailList = xml.getElementByTagName("eposta");
-	var izenList = xml.getElementByTagName("izena");
-	var ab1List = xml.getElementByTagName("abizena1");
-	var ab2List = xml.getElementByTagName("abizena2");
-	var telList = xml.getElementByTagName("telefonoa");
+	var xml = document.getElementById('erabxml').contentDocument;
+	var emailList = xml.getElementsByTagName("eposta");
+	var izenList = xml.getElementsByTagName("izena");
+	var ab1List = xml.getElementsByTagName("abizena1");
+	var ab2List = xml.getElementsByTagName("abizena2");
+	var telList = xml.getElementsByTagName("telefonoa");
 	var aurkitua = false;
 	for (var i = 0; i < emailList.length; i++) {
-		if (emailList[i].childNodes[0].nodeValue == document.getElementById("eposta").value) {
+		if (emailList[i].childNodes[0].nodeValue == document.getElementById("posta").value) {
 			aurkitua = true;
 				
 			document.getElementById("izena").style.display = "inline";
@@ -31,7 +17,8 @@ function bilatu() {
 			document.getElementById("abizena2").style.display = "inline";
 			document.getElementById("abizena2").value = ab2List[i].childNodes[0].nodeValue;
 			document.getElementById("telefonoa").style.display = "inline";
-			document.getElementById("telefonoa").value = telListList[i].childNodes[0].nodeValue;
+			document.getElementById("telefonoa").value = telList[i].childNodes[0].nodeValue;
+			break;
 		}
 	}
 	if (!aurkitua) {
@@ -41,4 +28,5 @@ function bilatu() {
 		document.getElementById("abizena2").style.display = "none";
 		document.getElementById("telefonoa").style.display = "none";
 	}
+
 }
