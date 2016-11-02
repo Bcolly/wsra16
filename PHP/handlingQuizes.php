f72759a8e5b19aa5a823c37eac16420906191884
Oier Beaskoetxea <obeaskoetxea@gmail.com>
2016-11-02 20:29:31 +0100

    lab6 bukatuta(errore bat zuzenduta)

--- PHP/handlingQuizes.php
+++ PHP/handlingQuizes.php
@@ -11,7 +11,7 @@
 					document.getElementById("galderaSartu").innerHTML= xhttp.responseText;
 				}
 			};
-				xhttp.open("GET","insertQuestion.php?"+"question="+ques+"&answer="+ans+"&zailtasuna="+zail+"&subject="+subj, true);
+				xhttp.open("GET","handlingQuizes.php?"+"question="+ques+"&answer="+ans+"&zailtasuna="+zail+"&subject="+subj, true);
 				xhttp.send();
 		}
 		function nireGalderak(){	
@@ -79,3 +79,70 @@
 		<p> <a href='../layout.html'>-=HOME=-</a> </p>
 </body>
 </html>
+
+<?php
+	if(isset($_GET['question'],$_GET['answer'])){
+		include "./konektatu.php";
+		
+		$eposta = $_SESSION['user'];
+		//SQL-n galdera txertatu
+		$sql = "INSERT INTO galdera(Galdera,Erantzuna,Zailtasuna,PostaElektronikoa) 
+		VALUES ('$_GET[question]' , '$_GET[answer]' , '$_GET[zailtasuna]', '$eposta')";
+		if (!$niremysqli->query($sql)){
+			echo "Taularen sorrerak huts egin: (" .
+			$mysqli->errno . ") " . $mysqli->error;
+			die('Errorea: ' . $niremysqli->error);
+		}
+		//XML-n galdera txertatu
+		$xml = simplexml_load_file('../XML/galderak.xml');
+		$galdera = $xml->addChild('assessmentItem');
+			$galdera->addAttribute('complexity', $_GET[zailtasuna]);
+			$galdera->addAttribute('subject', $_GET[subject]);
+		
+			$galderaT = $galdera->addChild('itemBody');
+				$galderaT->addChild('p',$_GET['question']);
+			$erantzuna = $galdera->addChild('correctResponse');
+				$erantzuna->addChild('value',$_GET[answer]);
+
+		if($xml->asXML("../XML/galderak.xml") === FALSE) {
+			echo "<br/>Galderak ez dira XML fitxategian gorde";
+		}
+		else {
+			echo "<br/><a href='./seeXMLQuestions.php'>Ikusi galderak XML bidez</a>";
+		}
+		
+		//konexioa datubasean gehitu
+		$aux = "SELECT IdKon,PostaElektronikoa FROM konexioak";
+		$giz = $niremysqli->query($aux);
+		
+		while($row = $giz->fetch_assoc()){  // taula osoa hautatu dugu bestela fetch_assoc-ek errorea ematen zuen:
+											// Fatal error: Call to a member function fetch_assoc() on boolean
+			if($row["PostaElektronikoa"]==$eposta){
+				$lerroak = $row["IdKon"];
+			}
+		}
+		echo $lerroak;
+		
+		$ekintza = "Galdera txertatu";
+		
+		if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+			$ip = $_SERVER['HTTP_CLIENT_IP'];
+		} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+			$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+		} else {
+			$ip = $_SERVER['REMOTE_ADDR'];
+		}
+		echo $ip;
+		$sqla = "INSERT INTO ekintzak(KonexioIdentitifikazioa,PostaElektronikoa,EkintzaMota,IPa)
+				VALUES('$lerroak', '$eposta', '$ekintza', '$ip')";
+		if (!$niremysqli->query($sqla)){
+			echo "Taularen sorrerak huts egin: (" .
+			$mysqli->errno . ") " . $mysqli->error;
+			die('Errorea: ' . $niremysqli->error);
+		}
+		else { echo "<p>Ekintza gehitu da</p>";}
+		
+		$niremysqli->close();
+	}
+
+?>
